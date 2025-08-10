c6cfb422b182220ce8715f69840974adf025095c
manhoosbilli1 <shoaib.mustafa7@hotmail.com>
2025-08-10 17:19:10 +0400

    added leader pv to get back to filesystem

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,7 +4,7 @@
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
